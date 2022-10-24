9807681c52b8148f6318411d0fe21dfe5169249b
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-10-24 17:06:43 -0400

    Added Greeting, prompt for string/integer.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
 namespace _1._1JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("    Welcome to the Dinosaur Database    ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }///////////////////////////////////////////////////////////////////////////////////
+        class Dinosaur
+        {
+            public string Name { get; set; }
+            public int Weight { get; set; }
+            public string DietType { get; set; }
+            public int EnclosureNumber { get; set; }
+            public string WhenAcquired { get; set; }
+        }
+
+
+
+        // static void Description()
+        // {
+        //     foreach (Dinosaur in Dinosaurs)
+        //     {
+        //         Console.WriteLine($"{Name} + {Weight} + {DietType} + {EnclosureNumber} + {WhenAcquired} ");
+        //     }
+        // }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            DisplayGreeting();
+
+            var Dinosaurs = new List<Dinosaur>();
+
+
+
+
+
+
+
+
+
+        }
     }
-  }
 }
